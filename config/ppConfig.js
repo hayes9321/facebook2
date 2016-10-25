9f1e2d6090ec2bcbaa042c3cb178e0dded31ebc5
hayes9321 <t.hayes8321@gmail.com>
2016-10-25 09:44:40 -0700

    changed PPconfig

--- config/ppConfig.js
+++ config/ppConfig.js
@@ -32,7 +32,7 @@ passport.use(new LocalStrategy({
 passport.use(new FacebookStrategy({
   clientID: process.env.FACEBOOK_APP_ID,
   clientSecret: process.env.FACEBOOK_APP_SECRET,
-  callbackURL: process.env.BASE_URL + '/auth/callback/facebook',
+  callbackURL: process.env.BASE_URL + 'auth/callback/facebook',
   profileFields: ['id', 'email', 'displayName', 'photos','posts','feed'],
   enableProof: true
 }, function(accessToken, refreshToken, profile, cb) {
